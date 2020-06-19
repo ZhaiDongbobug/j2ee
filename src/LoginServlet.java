aec0d95fd2a0d386206cea1aba2c52d92b09fa1a
ZhaiDongboBug <18747867831@163.com>
2020-06-19 10:47:11 +0800

    Servlet-跳转
    总结：
    
    调用HttpServletResponse.sendRedirect方法重定向的访问过程结束后，浏览器地址栏中显示的URL会发生改变，由初始的URL地址变成重定向的目标URL；而调用RequestDispatcher.forward
    方法的请求转发过程结束后，浏览器地址栏保持初始的URL地址不变

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -1,17 +1,49 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class LoginServlet extends HttpServlet {
+	//实例化
+	public LoginServlet() {
+		System.out.println("LoginServlet 构造方法 被调用");
+	}
+	//初始化
+	public void init(ServletConfig config) {
+		System.out.println("init(ServletConfig)");
+	}
+	
+	//提供服务
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		//获取中文的参数，如果不写这行代码，服务器显示乱码
+		request.setCharacterEncoding("UTF-8");
+		
 		String name = request.getParameter("name");
 		String password = request.getParameter("password");
 		
-		System.out.println("name:" + name);
-		System.out.println("password:" + password);
+		//System.out.println(name);
+		
+		String html = null;
+		
+		if("admin".equals(name)&&"123".equals(password)) {
+			request.getRequestDispatcher("success.html").forward(request, response);
+		}else {
+			response.sendRedirect("fail.html");
+		}
+		//返回中文的响应
+		response.setContentType("text/html; charset=UTF-8");
+		
+		PrintWriter pw = response.getWriter();
+		pw.println(html);
 	}
+	//销毁
+	public void destory() {
+		System.out.println("destory()");
+	}
+	
 }
